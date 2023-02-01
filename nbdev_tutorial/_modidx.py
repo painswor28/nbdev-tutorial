84fff04328977bb889da95cf3fb0d9f026d4b85f
painswor28 <painswor@nd.edu>
2023-02-01 12:37:24 -0800

    add card module

--- nbdev_tutorial/_modidx.py
+++ nbdev_tutorial/_modidx.py
@@ -5,4 +5,6 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://painswor28.github.io',
                 'git_url': 'https://github.com/painswor28/nbdev-tutorial',
                 'lib_path': 'nbdev_tutorial'},
-  'syms': {'nbdev_tutorial.core': {'nbdev_tutorial.core.foo': ('core.html#foo', 'nbdev_tutorial/core.py')}}}
+  'syms': { 'nbdev_tutorial.card': { 'nbdev_tutorial.card.Card': ('card.html#card', 'nbdev_tutorial/card.py'),
+                                     'nbdev_tutorial.card.Card.__init__': ('card.html#card.__init__', 'nbdev_tutorial/card.py'),
+                                     'nbdev_tutorial.card.Card.__str__': ('card.html#card.__str__', 'nbdev_tutorial/card.py')}}}
